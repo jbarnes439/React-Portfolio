63086e0b6c6eebfdb836b3765a7e83b582b7a136
Josh Barnes <jbarnes439@gmail.com>
2021-02-09 09:08:55 -0700

    Updated styling of font-sizes to be more responsive, added animation to project heading links, updated cursor on hover of toggle nav button

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,22 +4,22 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className='footer'>            
-            <a 
+            <a                 
                 href='https://www.linkedin.com/in/josh-barnes-developer/'
                 rel='noopener noreferrer' 
                 target='_blank'>
-                <img src='https://img.icons8.com/fluent/48/000000/linkedin.png' alt='linkedin icon'/>
+                <img src='https://img.icons8.com/fluent/48/000000/linkedin.png' alt='linkedin icon' className='footer__icon'/>
             </a>
             <a 
                 href='mailto:jbarnes439@gmail.com?subject=Loved%20your%20portfolio!'
                 className='email-link'>
                 Email: jbarnes439@gmail.com
             </a>
-            <a 
+            <a                 
                 href='https://github.com/jbarnes439' 
                 rel='noopener noreferrer' 
                 target='_blank'>
-                <img src='https://img.icons8.com/fluent/48/000000/github.png' alt='github icon'/>
+                <img src='https://img.icons8.com/fluent/48/000000/github.png' alt='github icon' className='footer__icon'/>
             </a>
         </footer>
     )
